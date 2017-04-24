d09c8f55dd659e65dba2ad784807c17d149698af
RenzhiDuan <1094602571@qq.com>
2017-04-24 14:16:18 +0800

    Add a new function add

--- EclipseGitLearning-1/main.cpp
+++ EclipseGitLearning-1/main.cpp
@@ -7,8 +7,18 @@
 
 #include <cstdio>
 
+//this function is added in v0.1
+int add(int a, int b){
+	return a+b;
+}
+
 int main(){
 	printf("Eclipse Git Learning 1");
+
+	int a,b;
+	scanf("%d %d", &a, &b);
+	printf("Sum: %d\n", add(a,b));
+
 }
 
 
